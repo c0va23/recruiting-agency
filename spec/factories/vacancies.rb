e744c4e845d0873ea67661245c243d09698d557f
Dmitrij Fedorenko <c0va23@gmail.com>
2016-04-25 15:48:57 +0300

    Add factory for vacancy and skill

---
+++ spec/factories/vacancies.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :vacancy do
+    name { FFaker::Company.name }
+    salary { rand(100_000..150_000) }
+    validity_days { rand(7..30) }
+    contacts { "#{FFaker::PhoneNumber.phone_number}, #{FFaker::Internet.email}" }
+  end
+end
