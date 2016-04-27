0bd9fadff60935e99dc01532b4c62ed75e949884
Dmitrij Fedorenko <c0va23@gmail.com>
2016-04-27 11:37:19 +0300

    Remove debug output

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -3,7 +3,6 @@ angular.module('app')
   return function(rawSkillIds) {
     // If skill_ids have one item, then wrap it into array
     var rawSkillIdsArray = ('string' == typeof rawSkillIds) ? [rawSkillIds] : rawSkillIds;
-    console.log(rawSkillIdsArray);
 
     // Convert string value into number
     return _(rawSkillIdsArray).map(function(skillIdRaw) {
