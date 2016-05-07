48455338d62ac308a09d70427b4206e74668d3a4
Dmitrij Fedorenko <c0va23@gmail.com>
2016-05-07 20:22:09 +0300

    Add checkbox "Partial match"

--- app/assets/javascripts/controllers/applicant_search_controller.js
+++ app/assets/javascripts/controllers/applicant_search_controller.js
@@ -16,6 +16,7 @@ angular.module('app')
     $scope.applicants = Applicant.search(params);
 
     $scope.query = {
+      partial_match: params.partial_match,
       salary: Number.parseFloat(params.salary),
       'skill_ids[]': _.map(params['skill_ids[]'], function(idStr){
         return Number.parseInt(idStr);
