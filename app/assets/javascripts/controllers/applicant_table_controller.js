c372aa31fb46a08fa864dd9442f3884601bef59a
Dmitrij Fedorenko <c0va23@gmail.com>
2016-05-08 20:50:00 +0300

    Fix highlight not matched attributes

--- app/assets/javascripts/controllers/applicant_table_controller.js
+++ app/assets/javascripts/controllers/applicant_table_controller.js
@@ -4,11 +4,16 @@ angular.module('app').controller('ApplicantTableController', [
     $scope
   ) {
     var query = angular.copy($scope.query);
+
     $scope.matchApplicantSalary = function(applicant) {
+      if(undefined == query) return undefined;
+
       return applicant.expected_salary < query.salary;
     };
 
     $scope.matchApplicantSkills = function(applicant) {
+      if(undefined == query) return undefined;
+
       var applicantSkillIds = _(applicant.skill_ids);
       return _(query['skill_ids[]']).every(function(expectedSkillId){
         return applicantSkillIds.includes(expectedSkillId);
--- app/assets/javascripts/controllers/vacancy_table_controller.js
+++ app/assets/javascripts/controllers/vacancy_table_controller.js
@@ -6,10 +6,14 @@ angular.module('app').controller('VacancyTableController', [
     var query = angular.copy($scope.query);
 
     $scope.matchVacancySalary = function(vacancy) {
+      if(undefined == query) return undefined;
+
       return vacancy.salary >= query.salary;
     };
 
     $scope.matchVacancySkills = function(vacancy) {
+      if(undefined == query) return undefined;
+
       var skillIds = _(query['skill_ids[]']);
       return _.every(vacancy.skill_ids, function(vacancySkillId){
         return skillIds.includes(vacancySkillId);
